2f3234e1aee525af52f43fb3b8b75e314a6186ef
Vitor Bahir <vitorb82014@gmail.com>
2023-03-29 03:46:26 -0300

    Initial implementation

---
+++ Course/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main()
+        {
+           HashSet<LogRecord> list = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using StreamReader sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(' ');
+                    string name = line[0];
+                    DateTime instant = DateTime.Parse(line[1]);
+                    list.Add(new LogRecord { Username = name, Instant = instant });
+                }
+
+                Console.WriteLine("Total users: " + list.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
